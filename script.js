d5eeaa4937451e084ddad5789a4710186f8d9bc0
nicnic2010 <145597535+nicnic2010@users.noreply.github.com>
2026-09-01 20:58:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,98 @@
+/* ========================================
+   QUIZ
+======================================== */
 
+
+/*
+Procura todos os botões que possuem
+a classe "resposta".
+*/
+
+const respostas =
+    document.querySelectorAll(
+        ".resposta"
+    );
+
+
+/*
+Procura o espaço onde o resultado
+será mostrado.
+*/
+
+const resultado =
+    document.querySelector(
+        "#resultado"
+    );
+
+
+/*
+Repete o código para cada botão
+encontrado.
+*/
+
+respostas.forEach(
+    function (botao) {
+
+
+        /*
+        Espera o usuário clicar
+        no botão.
+        */
+
+        botao.addEventListener(
+            "click",
+            function () {
+
+
+                /*
+                Descobre se a resposta
+                é certa ou errada.
+                */
+
+                const resposta =
+                    botao.dataset.resposta;
+
+
+                /*
+                Se a resposta for certa.
+                */
+
+                if (
+                    resposta === "certa"
+                ) {
+
+
+                    resultado.textContent =
+                        "🎉 Correto! O CSS é responsável pelas cores e aparência do site.";
+
+
+                    resultado.style.color =
+                        "green";
+
+
+                }
+
+
+                /*
+                Se a resposta for errada.
+                */
+
+                else {
+
+
+                    resultado.textContent =
+                        "❌ Essa não é a resposta. Tente novamente!";
+
+
+                    resultado.style.color =
+                        "#b32b5a";
+
+                }
+
+
+            }
+        );
+
+
+    }
+);
